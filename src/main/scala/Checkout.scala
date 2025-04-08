2d641b102061517521d6d659f11a60593e0b6aa3
akkum <akhilesh.kumar@digital.hmrc.gov.uk>
2025-04-08 13:41:29 +0100

    Added step -2

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,6 +1,6 @@
 package com.akkum
 
-sealed trait Item {
+sealed abstract trait Item {
   def name: String
   def price: BigDecimal
 }
@@ -9,23 +9,48 @@ case object Apple extends Item {
 
   override def price: BigDecimal = 0.60
   override def name: String = "Apple"
+
+  def buyOneGetOneFree (itemAmount: Int): BigDecimal = {
+    Math.ceil(itemAmount.toDouble / 2) * price
+  }
 }
 
 case object Orange extends Item {
 
   override def price: BigDecimal = 0.25
   override def name: String = "Orange"
+  
+  def threeForTwo (items : Seq[Item]) : BigDecimal = {
+    items.grouped(3).map {
+      group => group.take(2).map(_.price).sum
+    }
+  }.sum
 }
 
 class Checkout {
-  
+
   def calculate (items: Seq[String]) : BigDecimal = {
-    items.map {
-      case "Apple" => Apple.price
-      case "Orange" => Orange.price  
+    val itemList = items.map {
+      case "Apple" => Apple
+      case "Orange" => Orange
+    }
+    val grouped = itemList.groupBy(_.name)
+    applyOffers(grouped)
+
+  }
+
+  def applyOffers(itemsGroup: Map[String, Seq[Item]]) : BigDecimal = {
+
+    itemsGroup.map {
+      case ("Apple", appleItems) => {
+        Apple.buyOneGetOneFree(appleItems.size)
+        
+      }
+      case ("Orange", orangeItems) => Orange.threeForTwo(orangeItems)
+
     }.sum
   }
-  
+
 }
 
 
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -7,12 +7,17 @@ import org.scalatest.matchers.must.Matchers
 class ShoppingCartSpec extends AnyFreeSpec with Matchers  {
   "shopping Cart" - {
     val checkout = new Checkout
-    "should add a cost of two Apples and one Organges" in {
-      checkout.calculate(Seq("Apple", "Apple", "Orange")) mustBe 1.45
+    "should add a cost of two Apples and one Organges with Apple Offers" in {
+      checkout.calculate(Seq("Apple", "Apple", "Orange")) mustBe 0.85
     }    
-    "should add a cost of three Apples and four Organges" in {
-      checkout.calculate(Seq("Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange")) mustBe 2.80
+    "should add a cost of three Apples and four Organges with offers" in {
+      checkout.calculate(Seq("Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange")) mustBe 1.95
     }
+
+    "should add a cost of three Apples and five Organges with offers" in {
+      checkout.calculate(Seq("Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Orange")) mustBe 2.20
+    }
+    
 
   }
 }
